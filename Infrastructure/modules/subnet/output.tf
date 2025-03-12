582bec3ca984fb73818fb608ad253df6c1333caa
Mayur Manjrekar <mayurmanjrekar1999@gmail.com>
2025-03-12 17:44:48 +0530

    updated tf code

--- Infrastructure/modules/subnet/output.tf
+++ Infrastructure/modules/subnet/output.tf
@@ -1,5 +1,5 @@
 #subnet
 output "subnet" {
-  value       = google_compute_subnetwork.subnetwork
+  value       = google_compute_subnetwork.subnetwork.self_link
   description = "The created subnet resources"
 }
