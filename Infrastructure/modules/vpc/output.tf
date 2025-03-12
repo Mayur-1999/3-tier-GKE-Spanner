4a12915e7881cebce8d9d3a75f916507df7114ed
Mayur Manjrekar <mayurmanjrekar1999@gmail.com>
2025-03-12 17:18:31 +0530

    updated tf code

--- Infrastructure/modules/vpc/output.tf
+++ Infrastructure/modules/vpc/output.tf
@@ -1,5 +1,8 @@
 #vpc
-output "network" {
-  value = google_compute_network.network
-}
+# output "network" {
+#   value = google_compute_network.network
+# }
 
+output "network_self_link" {
+  value = google_compute_network.network.self_link
+}
