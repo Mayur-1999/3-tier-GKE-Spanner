097ae3bca736e1dda9d4146ae2f87207f5d4b03f
Mayur Manjrekar <mayurmanjrekar1999@gmail.com>
2025-03-20 11:51:20 +0530

    updated tf code

--- Infrastructure/Env/Dev/main.tf
+++ Infrastructure/Env/Dev/main.tf
@@ -100,7 +100,7 @@ module "gke_cluster" {
       initial_node_count = 1
       min_node_count     = 1
       max_node_count     = 4
-      locations          = ["us-central1-a", "us-central1-b"]
+      locations          = ["us-west1-a", "us-west1-b"]
       labels             = { os = "linux" }
       taint = [
         {
@@ -117,7 +117,7 @@ module "gke_cluster" {
       initial_node_count = 1
       min_node_count     = 1
       max_node_count     = 4
-      locations          = ["us-central1-a"]
+      locations          = ["us-west1-a"]
       labels             = { os = "linux" }
       taint              = []
     }
--- README.md
+++ README.md
@@ -147,7 +147,7 @@ echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.clou
 sudo apt-get update
 sudo apt-get install google-cloud-cli -y
 grep -rhE ^deb /etc/apt/sources.list* | grep "cloud-sdk"
-sudo apt-get update
+sudo apt-get update 
 sudo apt-get install -y kubectl
 sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin -y
 ```
