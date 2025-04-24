2928e1ea55bad9a5caf01928d2a2d930f4778ef3
Mayur Manjrekar <mayurmanjrekar1999@gmail.com>
2025-04-24 10:54:09 +0530

    updated terraform CD

--- Infrastructure/Env/Test/main.tf
+++ Infrastructure/Env/Test/main.tf
@@ -1,10 +1,1 @@
-resource "google_storage_bucket" "statefile-bucket" {
-  project                     = var.project_id
-  name                        = "test-qwiklabs-gcp-03-02605f581a7f"
-  location                    = var.region
-  force_destroy               = false
-  uniform_bucket_level_access = true
-  versioning {
-    enabled = true
-  }
-}
+
